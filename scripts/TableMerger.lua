57d1582b0bb73419f6294ab864bd40655ce21d7c
Adam <afellegithub@outlook.com>
2025-10-16 13:56:23 -0400

    Update TableMerger.lua

--- scripts/TableMerger.lua
+++ scripts/TableMerger.lua
@@ -1,6 +1,6 @@
 -- Adam Felle
 -- ModuleScript named "TableMerger" in ReplicatedStorage
--- Merges tables together as that is not default table behavior
+-- Merges tables together as that is not default table behavior.
 -- require(game.ReplicatedStorage.TableMerger).MergeTables({{1,2,3}, {4,5,6})
 
 local module = {}
