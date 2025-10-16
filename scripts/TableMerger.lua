42ef573032d5a3cb44243baef5828ebd7c18c0a8
Adam <afellegithub@outlook.com>
2025-10-16 13:51:24 -0400

    Update and rename ListMerger.lua to TableMerger.lua

---
+++ scripts/TableMerger.lua
@@ -0,0 +1,20 @@
+-- Adam Felle
+-- ModuleScript named "TableMerger" in ReplicatedStorage
+-- Merges tables together as that is not default table behavior
+-- require(game.ReplicatedStorage.TableMerger).MergeTables({{1,2,3}, {4,5,6})
+
+local module = {}
+
+function module.MergeTables(lists)
+	local mergedList = {}
+	
+	for _, list in lists do
+		for _, obj in list do
+			table.insert(mergedList, obj)
+		end
+	end
+	
+	return mergedList
+end
+
+return module
